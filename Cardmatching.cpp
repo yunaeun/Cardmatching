3c0e6f00f8185d934208011dca0adf08e8004660
yunaeun <naeun2964@naver.com>
2022-11-28 13:38:20 +0900

    Add: 카드매칭게임

---
+++ Cardmatching.cpp
@@ -0,0 +1,274 @@
+//Dev. BlockDMask
+//Blog. https://blockdmask.tistory.com/
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+using namespace std;
+#define INIT_POS 1
+//-----------------------------------------
+//�ܼ� ������ Ư�� ��ġ�� Ŀ���� �̵��ϴ� �Լ�.
+void GotoXY(int x, int y)
+{
+	COORD pos;
+	pos.X = 2 * x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+//�ܼ� â ũ��� ������ �����ϴ� �Լ�.
+void SetConsoleView()
+{
+	system("mode con:cols=65 lines=10");
+	system("title Card Matching Game. by BlockDmask.");
+}
+
+//Ű���� �Է� ���� �� �Էµ� Ű���带 ��ȯ�ϴ� �Լ�.
+int GetKeyDown()
+{
+	if (_kbhit() != 0)
+	{
+		return _getch();
+	}
+	return 0;
+}
+//-----------------------------------------
+//2���� �迭�� �̷���� ��
+const int originMap[4][4] = { {1, 6, 1, 8},
+								{6, 2, 5, 4},
+								{7, 2, 5, 4},
+								{8, 3, 3, 7} };
+
+//������ ����ü
+struct Pos
+{
+	int x;
+	int y;
+};
+
+//Ű���� �Է��� ������ ���� enum
+enum KEYBOARD
+{
+	IS_ARROW = 224,
+	UP = 72,
+	LEFT = 75,
+	RIGHT = 77,
+	DOWN = 80,
+	SPACE = 32,
+};
+
+//������ �����س��� enum
+enum COLOR
+{
+	GREEN = 10,
+	MINT,
+	RED,
+	PINK,
+	YELLOW,
+	WHITE,
+};
+
+//���� �Ŵ��� Ŭ����
+class GameManager
+{
+private:
+	char questionMap[4][4];	//���߰� �ִ� ��.
+	Pos player;				//�÷��̾� Ŀ�� ��ġ.
+	Pos flipPos[2];			//������ ī�� ��ġ ����.
+	int flipCount;			//������ ī�� ����.
+	int spaceCount;			//������ Ƚ��.
+	int matchedCardCount;	//���� ī�� ����.
+public:
+	void StartGame()
+	{
+		Init();		//���� �ʱ�ȭ
+		DrawBack();	//��� �׸���
+
+		//���� ���� ����
+		int key = 0;
+		while (true)
+		{
+			key = GetKeyDown();	//Ű �Է� �޴� �Լ�
+			if (key == KEYBOARD::IS_ARROW)
+			{
+				//ȭ��ǥ��.
+				MovePos();
+			}
+			if (key == KEYBOARD::SPACE)
+			{
+				//�����̽�
+				FlipCard();
+			}
+			//draw
+			DrawMain();
+			Sleep(100);
+			CheckCard();
+		}
+	}
+
+	//�� �ʱ�ȭ
+	void Init()
+	{
+		for (int y = 0; y < 4; ++y)
+		{
+			for (int x = 0; x < 4; ++x)
+			{
+				questionMap[y][x] = '?';	//����ǥ�� �� ����
+			}
+		}
+		player.x = 0;
+		player.y = 0;
+		flipCount = 0;
+		spaceCount = 0;
+		matchedCardCount = 0;
+	}
+
+	//ī�� ������ �Լ�
+	void FlipCard()
+	{
+		if (questionMap[player.y][player.x] != '?') return;
+		if (flipCount > 2) return;
+		flipPos[flipCount].x = player.x;
+		flipPos[flipCount].y = player.y;
+		questionMap[player.y][player.x] = originMap[player.y][player.x] + '0';
+		++flipCount;
+	}
+
+	//Ŀ���� ��ġ �̵�
+	void MovePos()
+	{
+		GotoXY(INIT_POS + (player.x * 3), INIT_POS + (player.y * 2) + 1);
+		printf("  ");
+
+		//Ű���� �Է��� �޾Ƽ�, �ش� ��ġ�� �̵��� �����ش�.
+		switch (_getch())
+		{
+		case KEYBOARD::LEFT:
+			--player.x;
+			if (player.x <= 0)
+			{
+				player.x = 0;
+			}
+			break;
+		case KEYBOARD::RIGHT:
+			++player.x;
+			if (player.x >= 3)
+			{
+				player.x = 3;
+			}
+			break;
+		case KEYBOARD::UP:
+			--player.y;
+			if (player.y <= 0)
+			{
+				player.y = 0;
+			}
+			break;
+		case KEYBOARD::DOWN:
+			++player.y;
+			if (player.y >= 3)
+			{
+				player.y = 3;
+			}
+			break;
+		}
+	}
+
+	void DrawMain()
+	{
+		//map draw
+		SetTextColor(WHITE);
+		char c = '\0';
+		for (int y = 0; y < 4; ++y)
+		{
+			GotoXY(INIT_POS, INIT_POS + (2 * y));
+			for (int x = 0; x < 4; ++x)
+			{
+				c = questionMap[y][x];
+				if (c != '?')
+				{
+					SetTextColor(GREEN);
+				}
+				printf("[%c]   ", c);
+				SetTextColor(WHITE);
+			}
+		}
+
+		//player draw
+		GotoXY(INIT_POS + (player.x * 3), INIT_POS + (player.y * 2) + 1);
+		printf(" ^");
+
+		GotoXY(14, 1);
+		printf("Count : %d", spaceCount);
+	}
+
+	void CheckCard()
+	{
+		if (flipCount == 2)
+		{
+			char c = '?';
+			if (originMap[flipPos[0].y][flipPos[0].x] == originMap[flipPos[1].y][flipPos[1].x])
+			{
+				//������
+				c = originMap[flipPos[0].y][flipPos[0].x] + '0';
+				++matchedCardCount;
+			}
+			else
+			{
+				//�ٸ���.
+				Sleep(500);
+			}
+			questionMap[flipPos[0].y][flipPos[0].x] = c;
+			questionMap[flipPos[1].y][flipPos[1].x] = c;
+			flipCount = 0;
+			++spaceCount;
+
+			if (matchedCardCount >= 8)
+			{
+				DrawGameClear();
+				Sleep(2000);
+				system("cls");
+				Init();
+				DrawBack();
+			}
+		}
+	}
+
+	void DrawBack()
+	{
+		SetTextColor(WHITE);
+		GotoXY(14, 6);
+		cout << "https://blockdmask.tistory.com/";
+		GotoXY(14, 7);
+		cout << "_by. ";
+		SetTextColor(PINK);
+		cout << "BlockDMask";
+		SetTextColor(WHITE);
+	}
+
+	void DrawGameClear()
+	{
+		SetTextColor(YELLOW);
+		GotoXY(10, 3);
+		cout << "=========================";
+		GotoXY(10, 4);
+		cout << "======= C L E A R =======";
+		GotoXY(10, 5);
+		cout << "=========================";
+		SetTextColor(WHITE);
+	}
+
+	void SetTextColor(int color)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+	}
+};
+
+//�����Լ�
+int main(void)
+{
+	SetConsoleView();
+	GameManager* pGameMgr = new GameManager();
+	pGameMgr->StartGame();
+	delete(pGameMgr);
+	return 0;
+}
